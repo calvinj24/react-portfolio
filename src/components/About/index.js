f93fce1711e916b6cbe0a02b47dd2e5d0fb1ffbc
Calvin Johnson <calvinjohnson@Calvins-MacBook-Pro-7.local>
2021-04-19 16:15:48 -0500

    add intial about section content

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <div className="intro-container">
+        <div className="greeting">
+          <h2>Hi there!</h2>
+          <h1>I'm Cal Johnson</h1>
+          <p>A front-end web developer.</p>
+        </div>
+        <div className="cta">
+          <button className="contact-btn">Contact Me</button>
+          <button className="my-work-btn">My Work</button>
+        </div>
+        <div className="intro">
+          <p>
+            Magna minim ut cillum id labore ea. Voluptate laboris sint minim sunt ex eu nostrud non quis anim eu officia. Cillum aliquip dolor ex reprehenderit consectetur voluptate duis occaecat dolore incididunt. Tempor consectetur Lorem nisi deserunt. Qui occaecat sit ullamco dolor ullamco Lorem ullamco consectetur quis et et. Est dolor quis aliqua anim nostrud nostrud esse culpa enim magna nisi cillum minim ut. Laborum duis enim ut ipsum anim dolore.
+          </p>
+        </div>  
+      </div>
+      <div className="headshot">
+
+      </div>
+    </section>
+  )
+}
+
+export default About;

cad5b32beae58dd00f2e8cc537ac1744bfa5c1ae
Calvin Johnson <calvinjohnson@Calvins-MacBook-Pro-7.local>
2021-06-02 18:01:11 -0500

    add initial resume info

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,7 +4,29 @@ function Resume() {
 
   return (
     <section>
-      <a href="/files/Calvin Johnson Resume.pdf" target="_blank" download>Download</a>
+      <div className="contact-info">
+        <h2>Calvin Johnson</h2>
+        <p> 3603 Padre Ct, Nisswa, MN 56468</p>
+        <p>(218) 820 4013</p>
+        <p>joh11145@umn.edu</p>
+      </div>
+      <div className="summary">
+        <h3>Summary</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate sapien suscipit dolor pretium, sed pellentesque ex facilisis. Phasellus imperdiet lacinia faucibus. Sed eu risus in leo egestas sagittis. Suspendisse vestibulum mauris eu mattis vestibulum. Mauris tristique semper varius. Fusce facilisis lectus id facilisis cursus. Donec tincidunt dapibus aliquet. Vestibulum ac porta tellus, id posuere diam. Suspendisse elementum lorem non magna porttitor, sit amet ultrices lacus consectetur.</p>
+      </div>
+      <div className="education">
+        <h3>Education</h3>
+        <p>
+          <b>University of Minnesota</b> <br/>
+          Curtis L. Carlson School of Management <br/>
+          Bachelor of Science in Business <br/>
+          Major: Finance <br/>
+        </p>
+      </div>
+      <div className="experience">
+        <h3>Experience</h3>
+      </div>
+
     </section>
   )
 }

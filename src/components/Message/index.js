3e2aaf1a20130c029e0aa30bb94143db864accfb
Calvin Johnson <calvinjohnson@Calvins-MacBook-Pro-7.local>
2021-06-02 23:27:52 -0500

    add animation to header

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { useSpring, animated } from 'react-spring'
 
 function Message(props) {
   const {currentPage} = props
+  const spring = useSpring({ 
+    to: { opacity: 1 }, 
+    from: { opacity: 0 }, 
+    reset: true
+  })
 
   return (
     <div className="message ft-light">
 
       {currentPage==="about" &&
-        <div className="greeting">
-          <h2>Hi there!</h2>
-          <h1>I'm Cal Johnson</h1>
-          <p>A front-end web developer.</p>
-          <div className="cta">
-            <button className="contact-btn">Contact Me</button>
-            <button className="my-work-btn">My Work</button>
+        <animated.div style={spring}>
+          <div className="greeting">
+            <h2>Hi there!</h2>
+            <h1>I'm Cal Johnson</h1>
+            <p>A front-end web developer.</p>
+            <div className="cta">
+              <button className="contact-btn">Contact Me</button>
+              <button className="my-work-btn">My Work</button>
+            </div>
           </div>
-        </div>
+        </animated.div>
       }
 
     </div>
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Modal({ onClose, currentProject }) {
   const { name, description, github, deployment } = currentProject;
